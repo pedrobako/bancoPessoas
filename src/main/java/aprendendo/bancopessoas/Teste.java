820c7bcebeb5c1513a3996d0696e9d74e128ea35
pedrobako <pedrobako@hotmail.com>
2023-06-26 17:35:45 -0300

    Testando git

--- src/main/java/aprendendo/bancopessoas/Teste.java
+++ src/main/java/aprendendo/bancopessoas/Teste.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package aprendendo.bancopessoas;
 
 /**
  *
  * @author pedro
  */
-public class BancoPessoas {
-
-    public static void main(String[] args) {
-        System.out.println("Teste!");
-    }
+public class Teste {
+    
 }
